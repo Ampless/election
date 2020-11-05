2cbb1730ac69db680d423f588e34e9eefbbe0440
Chris <chrissx@chrissx.de>
2020-11-05 03:42:13 +0100

    started writing more of the new cpp implementation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <sleepy_discord/sleepy_discord.h>
+#include <cpr/cpr.h>
+
+#define API_ENDPOINT "https://www.nbcnews.com/politics/2020-elections/president-results?format=json"
+
+std::string getElectionMessage() {
+        cpr::Response resp = cpr::Get(cpr::Url{API_ENDPOINT});
+        return resp.text;
+}
 
 class ElectionBot : public SleepyDiscord::DiscordClient {
 public:
-	using SleepyDiscord::DiscordClient::DiscordClient;
-	void onMessage(SleepyDiscord::Message message) override {
-		if (message.startsWith("whcg hello"))
-			sendMessage(message.channelID, "Hello " + message.author.username);
-	}
+        using SleepyDiscord::DiscordClient::DiscordClient;
+        void onMessage(SleepyDiscord::Message message) override {
+                if (!message.startsWith("!e")) return;
+                sendMessage(message.channelID, getElectionMessage());
+        }
 };
 
-int main() { ElectionBot("secretthisisatest").run(); }
+int main() {
+        std::ifstream secretfile{"../.secret"};
+        std::string secret;
+        secretfile >> secret;
+        ElectionBot(secret).run();
+}
