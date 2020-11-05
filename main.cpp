5ee661b7105056f88f19b7d7b37767f5ec0a68d5
Chris <chrissx@chrissx.de>
2020-11-05 02:48:34 +0100

    started getting a cpp implementation to work

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <sleepy_discord/sleepy_discord.h>
+
+class ElectionBot : public SleepyDiscord::DiscordClient {
+public:
+	using SleepyDiscord::DiscordClient::DiscordClient;
+	void onMessage(SleepyDiscord::Message message) override {
+		if (message.startsWith("whcg hello"))
+			sendMessage(message.channelID, "Hello " + message.author.username);
+	}
+};
+
+int main() { ElectionBot("secretthisisatest").run(); }
